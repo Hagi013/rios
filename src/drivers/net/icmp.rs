d3285d7ee8e5dd9f8bcabe86cfd4a836b34cf9b8
Hagi013 <generativepassion@gmail.com>
2021-11-12 19:45:09 +0900

    temp commit

---
+++ src/drivers/net/icmp.rs
@@ -0,0 +1,99 @@
+use alloc::vec::Vec;
+use crate::memory::dma::DmaBox;
+
+#[repr(u8)]
+#[derive(Copy, Clone)]
+enum IcmpEchoType {
+    EchoReplyMessage = 0,
+    DestinationUnreachableMessage =  3,
+    SourceQuenchMessage = 4,
+    RedirectMessage = 5,
+    EchoMessage = 8,
+    RouterAdvertisementMessage = 9,
+    RouterSolicitationMessage = 10,
+    TimeExceededMessage = 11,
+    ParameterProblemMessage = 12,
+    TimestampMessage = 13,
+    TimestampReplyMessage = 14,
+    InformationRequestMessage = 15,
+    InformationReplyMessage = 16,
+    AddressMaskRequestMessage = 17,
+    AddressMaskReplyMessage = 18,
+    Traceroute = 30,
+}
+
+#[repr(C)]
+struct IcmpHeader {
+    icmp_type: IcmpEchoType,
+    icmp_code: u8,
+    checksum: u16,
+}
+
+#[repr(C)]
+struct EchoMessage {
+    icmp_header: IcmpHeader,
+    identifier: u16,
+    sequence_num: u16,
+    data: DmaBox<[u8]>,
+}
+
+impl EchoMessage {
+    pub fn new() -> Self {
+        let s: &[u8] = &[];
+        EchoMessage {
+            icmp_header: IcmpHeader {
+                icmp_type: IcmpEchoType::EchoMessage,
+                icmp_code: 0x0,
+                checksum: 0x0,
+            },
+            identifier: 0x0,
+            sequence_num: 0x0,
+            data: DmaBox::from(s),
+        }
+    }
+
+    fn to_slice(&self) -> DmaBox<[u8]> {
+        let mut slice: &[u8] = &[
+            self.icmp_header.icmp_type as u8,
+            self.icmp_header.icmp_code,
+        ];
+        let slice = &[&slice[..], &self.icmp_header.checksum.to_be_bytes()].concat();
+        let slice = &[&slice[..], &self.identifier.to_be_bytes()].concat();
+        let slice = &[&slice[..], &self.sequence_num.to_be_bytes()].concat();
+        let s: &[u8]  = &[&slice[..], &self.data[..]].concat();
+        DmaBox::from(s)
+
+    }
+
+    // チェックサムは、ICMPタイプから始まるICMPメッセージの1の補数の合計について、16ビットの1の補数を取ったものである。
+    // チェックサム計算中は、チェックサムフィールドを0にする。このチェックサムは将来置き換えられる可能性がある。
+    fn calc_checksum(&mut self) {
+        let slice: &[u16] = &[
+            (self.icmp_header.icmp_type as u8 as u16) << 8 | self.icmp_header.icmp_code as u16,
+            self.icmp_header.checksum,
+            self.identifier,
+            self.sequence_num,
+        ];
+        let mut data_u16_list: Vec<u16> = vec![];
+        for idx in 0..self.data.len() / 2 + 1 {
+            if idx * 2 >= self.data.len() {
+                continue;
+            }
+            if idx * 2 + 1 >= self.data.len() {
+                // data = &[&data[..], &[(self.data[idx * 2] as u16) << 8]].concat();
+                data_u16_list.push((self.data[idx * 2] as u16) << 8);
+                continue;
+            }
+            // data = &[&data[..], &[((self.data[idx * 2] as u16) << 8 | self.data[idx * 2 + 1] as u16)]].concat();
+            data_u16_list.push(((self.data[idx * 2] as u16) << 8 | self.data[idx * 2 + 1] as u16));
+        }
+        let slice: &[u16] = &[&slice[..], data_u16_list.as_slice()].concat();
+        let sum: u32 = slice.iter().fold(0, |acc, &x| { acc + (x as u32) });
+        let upper: u16 = (sum >> 8) as u16;
+        let bottom: u16 = (sum & 0x0000ffff) as u16;
+        self.icmp_header.checksum = bottom + upper;
+    }
+}
+
+
+
---
+++ src/drivers/net/ip.rs
@@ -0,0 +1,156 @@
+use alloc::borrow::ToOwned;
+use alloc::vec::Vec;
+use alloc::string::String;
+
+use super::e1000::{get_mac_addr, e1000_send_packet};
+use super::net_util::{switch_endian16, any_as_u8_vec, push_to_vec};
+use crate::memory::dma::DmaBox;
+use crate::arp;
+
+use crate::arch::graphic::{Graphic, Printer, print_str};
+use core::fmt::Write;
+
+#[repr(u8)]
+#[derive(Clone, Copy)]
+enum VersionIhl {
+    Ip = 0x45, // Internet Protocol
+    St = 0x55, // ST Datagram Mode
+    Ipv6 = 0x65, // Internet Protocol version 6
+    TpIx = 0x75, // TP/IX: The Next Internet
+    Pip = 0x85, // The P Internet Protocol
+    Tuba = 0x95, // TUBA
+}
+
+#[repr(u8)]
+#[derive(Clone, Copy)]
+pub enum IpProtocol {
+    Icmp = 0x01,
+    Tcp = 0x06,
+    Udp = 0x07,
+}
+
+
+#[repr(C)]
+pub struct IpHdr {
+    version_ihl: VersionIhl,
+    escp_ecn: u8,
+    length: u16,
+    identifier: u16,
+    flag_flagment_offset: u16,
+    ttl: u8,
+    protocol: IpProtocol,
+    checksum: u16,
+    src_ip_addr: [u8; 4],
+    dst_ip_addr: [u8; 4],
+    payload: DmaBox<[u8]>,
+}
+
+impl IpHdr {
+    fn new() -> IpHdr {
+        IpHdr {
+            version_ihl: VersionIhl::Ip,
+            escp_ecn: 0x00,
+            length: 0x00,
+            identifier: 0x00,
+            flag_flagment_offset: 0x00,
+            ttl: 30,
+            protocol: IpProtocol::Tcp,
+            checksum: 0x00,
+            src_ip_addr: [0x00, 0x00, 0x00, 0x00],
+            dst_ip_addr: [0x00, 0x00, 0x00, 0x00],
+            payload: DmaBox::from(&[]),
+        }
+    }
+}
+
+impl IpHdr {
+    fn to_slice(&self) -> DmaBox<[u8]> {
+        let slice: &[u8] = &[(self.version_ihl as u8).to_be_bytes()].concat();
+        let slice: &[u8] = &[&slice[..], &self.escp_ecn.to_be_bytes()].concat();
+        let slice: &[u8] = &[&slice[..], &self.length.to_be_bytes()].concat();
+        let slice: &[u8] = &[&slice[..], &self.identifier.to_be_bytes()].concat();
+        let slice: &[u8] = &[&slice[..], &self.flag_flagment_offset.to_be_bytes()].concat();
+        let slice: &[u8] = &[&slice[..], &self.ttl.to_be_bytes()].concat();
+        let slice: &[u8] = &[&slice[..], &(self.protocol as u8).to_be_bytes()].concat();
+        let slice: &[u8] = &[&slice[..], &self.checksum.to_be_bytes()].concat();
+        let slice: &[u8] = &[&slice[..], &self.src_ip_addr[..]].concat();
+        let slice: &[u8] = &[&slice[..], &self.dst_ip_addr[..]].concat();
+        let s: &[u8] = &[&slice[..], &self.payload[..]].concat();
+        DmaBox::from(s)
+    }
+
+    pub fn get_offset(&self) -> bool {
+        (self.flag_flagment_offset) & 0b1111111111111000 == 0b010
+    }
+
+    pub fn check_fragment_on(&self) -> bool {
+        0b010 & (self.flag_flagment_offset) == 0b010
+    }
+
+    pub fn fragment_on(&mut self) {
+        self.flag_flagment_offset = self.flag_flagment_offset | 0b0000000000000010
+    }
+
+    pub fn fragment_off(&mut self) {
+        self.flag_flagment_offset = self.flag_flagment_offset & 0b1111111111111101
+    }
+
+    pub fn check_last_packet(&self) -> bool {
+        0b100 & (self.flag_flagment_offset) == 0b100
+    }
+
+    pub fn last_packet_on(&mut self) {
+        self.flag_flagment_offset = self.flag_flagment_offset | 0b0000000000000100
+    }
+
+    pub fn get_data(&self) -> DmaBox<[u8]> {
+        self.payload.clone()
+    }
+
+    pub fn calc_checksum(&mut self) {
+        // u8をu16に合わせる際に小さいアドレスの方を8ビット右シフトしているのは、それでビッグエンディアンになるから
+        let slice: &[u16] = &[
+            (self.version_ihl as u8 as u16) << 8 | (self.escp_ecn as u16),
+            self.length,
+            self.identifier,
+            self.flag_flagment_offset,
+            (self.ttl as u16) << 8 | (self.protocol as u16),
+            self.checksum,
+            (self.src_ip_addr[0] as u16) << 8 | (self.src_ip_addr[1] as u16),
+            (self.src_ip_addr[2] as u16) << 8 | (self.src_ip_addr[3] as u16),
+            (self.dst_ip_addr[0] as u16) << 8 | (self.dst_ip_addr[1] as u16),
+            (self.dst_ip_addr[2] as u16) << 8 | (self.dst_ip_addr[3] as u16),
+        ];
+        let mut payload_u16: Vec<u16> = vec![];
+        for idx in 0..self.payload.len() / 2 + 1 {
+            if idx * 2 >= self.payload.len() { continue; }
+            if idx * 2 + 1 >= self.payload.len() {
+                // slice = &[slice[..].as_ref(), &[(self.payload[idx * 2] as u16) << 8]].concat();
+                payload_u16.push((self.payload[idx * 2] as u16) << 8);
+                continue;
+            }
+            // slice = &[&slice[..], &[(self.payload[idx * 2] as u16) << 8 | (self.payload[idx * 2 + 1] as u16)]].concat();
+            payload_u16.push((self.payload[idx * 2] as u16) << 8 | (self.payload[idx * 2 + 1] as u16));
+        }
+        let slice: &[u16] = &[&slice[..], payload_u16.as_slice()].concat();
+        let sum: u32 = slice.iter().fold(0, |acc, &cur| { acc + (cur as u32) });
+        self.checksum = ((0x0000ffff & sum) as u16 + (sum >> 8) as u16) as u16
+    }
+}
+
+pub fn send_ip_packet(protocol: IpProtocol, dst_ip_addr: &[u8; 4], payload: DmaBox<[u8]>) -> Result<(), String> {
+    let (my_hardware_addr, my_ip_addr) = arp::get_my_hard_and_ip_addr();
+    let mut ip = IpHdr {
+        version_ihl: VersionIhl::Ip,
+        escp_ecn: 0x00,
+        length: 0x00,
+        identifier: 0x00,
+        flag_flagment_offset: 0x00,
+        ttl: 30,
+        protocol,
+        checksum: 0x00,
+        src_ip_addr: [0x00, 0x00, 0x00, 0x00],
+        dst_ip_addr: [dst_ip_addr[0], dst_ip_addr[1], dst_ip_addr[2], dst_ip_addr[3]],
+        payload,
+    };
+}
